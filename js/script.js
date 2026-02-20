64ebafc9d3d55e54656cfd3d97631d67e133a590
Daniel Wang <daniel22373@gmail.com>
2026-02-20 14:35:58 -0500

    Filter weather data by date, clean up code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-// Specify margin.
-const margin = {
-    top: 40,
-    right: 40,
-    bottom: 100,
-    left: 60
-}
-
+// Specify the margin, width, and ehight of the temperature plot.
+const margin = {top: 40, right: 40, bottom: 60, left: 60}
 const width = 600 - margin.left - margin.right
 const height = 400 - margin.top - margin.bottom
 
@@ -18,18 +12,43 @@ function parseDate(date) {
     return new Date(year, month, day);
 }
 
+// Returns a Date object that is dateIdx days away from Jaunary 1st, 2017.
+function dateIndexToDate(dateIdx) {
+    // Get the start date.
+    const targetDate = new Date(startDate);
+
+    // Add the number of days in the index.
+    targetDate.setDate(targetDate.getDate() + dateIdx);
+
+    // Return the target date.
+    return targetDate;
+}
+
+// Filters the weather data to only get values from a specific date.
+function filterByDate(weatherData, date) {
+    return weatherData.filter(d =>
+        d.date.getFullYear() === date.getFullYear() &&
+        d.date.getMonth() === date.getMonth() &&
+        d.date.getDate() === date.getDate()
+    );
+}
+
 // Initialize the variable to store the data.
-let weather_data = [];
+let weatherData = [];
+// Start date is January 1st, 2017 (months are zero-indexed).
+const startDate = new Date(2017, 0, 1, 0, 0, 0, 0);
+// Number of days past the start date to show data from.
+let currDateIdx = 0;
 
 // Bounding latitude and longitude points for contiguous U.S. (excluding Hawaii, Alaska, and territories).
-const south_bound = 24.52;
-const north_bound = 49.38;
-const west_bound = -124.78;
-const east_bound = -66.95;
+const southBound = 24.52;
+const northBound = 49.38;
+const westBound = -124.78;
+const eastBound = -66.95;
 
 // Min and max average temperatures.
-const min_temp = -20.56;
-const max_temp = 105.26;
+const minTemp = -20.56;
+const maxTemp = 105.26;
 
 // Load the data.
 d3.csv('data/weather.csv').then(data => {
@@ -69,8 +88,8 @@ d3.csv('data/weather.csv').then(data => {
     return data;
 }).then(data => {
     // Filter for values in contiguous U.S. (excluding Hawaii, Alaska, and territories),
-    data = data.filter(d => (d.latitude >= south_bound) && (d.latitude <= north_bound));
-    data = data.filter(d => (d.longitude >= west_bound) && (d.longitude <= east_bound));
+    data = data.filter(d => (d.latitude >= southBound) && (d.latitude <= northBound));
+    data = data.filter(d => (d.longitude >= westBound) && (d.longitude <= eastBound));
     
     console.log("Filtered for contiguous U.S.");
 
@@ -79,16 +98,21 @@ d3.csv('data/weather.csv').then(data => {
     console.log("Min avg temp:", Math.min(...data.map(d => d.temp)));
     console.log("Max avg temp:", Math.max(...data.map(d => d.temp)));
 
+    // Check the min and max dates.
+    // Min and max dates are January 1st, 2017 and September 17th, 2017.
+    console.log("Min date:", new Date(Math.min(...data.map(d => d.date))));
+    console.log("Max date:", new Date(Math.max(...data.map(d => d.date))));
+
     return data;
 }).then(data => {
     // Store the data.
-    weather_data = data;
+    weatherData = data;
 
-    console.log("Loaded data:", weather_data);
-    console.log(weather_data.length, "values loaded.")
+    console.log("Loaded data:", weatherData);
+    console.log("Number of values loaded:", weatherData.length);
 
     // Update the visualization.
-    updateVis();
+    updateVis(currDateIdx);
 });
 
 // Create svg and g
@@ -101,15 +125,16 @@ const svg = d3.select('#vis')
 
 // Create scales.
 const xScale = d3.scaleLinear()
-    .domain([west_bound, east_bound]) // predefined data range
+    .domain([westBound, eastBound]) // predefined data range
     .range([0, width]);
 
 const yScale = d3.scaleLinear()
-    .domain([south_bound, north_bound]) // predefined data range
+    .domain([southBound, northBound]) // predefined data range
     .range([height, 0]);
 
-const colorScale = d3.scaleSequential(d3.interpolateRdYlBu)
-    .domain([100, -20]);
+const colorScale = d3.scaleDiverging()
+  .domain([maxTemp, 50, minTemp]) // [minimum, center/zero, maximum]
+  .interpolator(d3.interpolateRdYlBu);
 
 // Add axes.
 const xAxis = d3.axisBottom(xScale)
@@ -124,11 +149,17 @@ svg.append('g')
     .attr('class', 'y-axis')
     .call(yAxis);
 
-// Display the data.
+// Updates the visualization to show temperature data from a specific data.
+function updateVis(currDateIdx) {
+    // Filter weatherData to get data from the specified date.
+    let currDate = dateIndexToDate(currDateIdx);
+    const filteredData = filterByDate(weatherData, currDate);
 
-function updateVis() {
+    console.log("Filtered for values from date:", currDate);
+
+    // Update the visualization.
     svg.selectAll('.point')
-    .data(weather_data)
+    .data(filteredData)
     .join(
         enter => {
             return enter
@@ -151,7 +182,6 @@ function updateVis() {
 }
 
 // Create functions to update data (from lab 3)
-
 function addRandomPoint() {
     // for debugging
     console.log('add point')
@@ -195,7 +225,7 @@ d3.select('#removePoint')
 d3.select('#updatePoints')
     .on('click', updateRandomPoints);
 
-// Add labels
+// Add labels for the axes.
 svg.append('text')
 .attr('class', 'axis-label')
 .attr('x', width / 2)
@@ -211,23 +241,28 @@ svg.append('text')
 .style('text-anchor', 'middle')
 .text('Latitude');
 
-// Add a legend
+// Add a legend.
 const legendWidth = 200;
 const legendHeight = 10;
 
-const legend = svg.append('g')
-    .attr('class', 'legend')
-    .attr('transform', `translate(0, ${height + 50})`);
+// Create a SVG for the legend below the plot.
+const legendSvg = d3.select('#legend')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', 80);   // controls spacing below the axis label
 
-// Add a definition for the gradient
-const defs = svg.append("defs");
+const legend = legendSvg.append('g')
+    .attr('transform', `translate(${(width - legendWidth)/2 + margin.left}, 25)`);
+
+// Add a definition for the gradient.
+const defs = legendSvg.append("defs");
 
 const linearGradient = defs.append("linearGradient")
     .attr("id", "temperature-gradient")
     .attr("x1", "0%").attr("y1", "0%")
     .attr("x2", "100%").attr("y2", "0%");
 
-// Define the colors 
+// Define the colors .
 linearGradient.selectAll("stop")
     .data([
         {offset: "0%", color: d3.interpolateRdYlBu(1)},     // blue
@@ -238,28 +273,28 @@ linearGradient.selectAll("stop")
     .attr("offset", d => d.offset)
     .attr("stop-color", d => d.color);
 
-// Draw the bar
+// Draw the bar.
 legend.append("rect")
     .attr("width", legendWidth)
     .attr("height", legendHeight)
     .style("fill", "url(#temperature-gradient)");
 
-// Create a scale for the legend axis
+// Create a scale for the legend axis/
 const legendScale = d3.scaleLinear()
-    .domain([min_temp, max_temp])
+    .domain([minTemp, maxTemp])
     .range([0, legendWidth]);
 
-// Add the axis
+// Add the axis.
 const legendAxis = d3.axisBottom(legendScale).ticks(5);
 
 legend.append("g")
     .attr("transform", `translate(0, ${legendHeight})`)
     .call(legendAxis);
 
-// Add a label for the legend
+// Add a label for the legend.
 legend.append("text")
     .attr("x", 0)
-    .attr("y", -5) // Position slightly above the bar
+    .attr("y", -5)
     .style("font-size", "12px")
     .style("font-weight", "bold")
     .text("Temperature (°F)");
